82d82f1f9bfafc7c73c2022f0eba2bae3842b97a
uncoder <huiqiang.shi@wenba100.com>
2017-09-01 11:13:07 +0800

    更新

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+
 module.exports = function (source) {
 	// 启用缓存
 	this.cacheable && this.cacheable();
+
 	// 做一些处理
 	var reg = /\ssrc.*\.\w{2,3}/g;
 	var result = source.replace(reg, function (args) {
@@ -10,10 +12,13 @@ module.exports = function (source) {
 		var name = fileUrl[1].slice(1);
 		return attrString + '=' + fileUrl[1].slice(0, 1) + name + '?version=' + timestamp;
 	});
+
 	// 同步字符串处理，直接return返回
 	return "module.exports = " + JSON.stringify(result);
+
 	// 若需要配合，返回多个数据给下一个loader
 	// this.callback(null, source, other);
+
 	// 若需要异步
 	// var callback = this.async();
 	// doSomething(content, function(err, result) {
